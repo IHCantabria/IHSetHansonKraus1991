9fa6996bf5358d87659368896a140b881d98e5cc
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-04-03 09:47:54 +0200

    Starting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetHansonKraus1991',
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -11,7 +11,8 @@ setup(
         'numba',
         'datetime',
         'spotpy',
-        'IHSetCalibration @ git+https://github.com/defreitasL/IHSetHansonKraus1991.git'
+        'IHSetCalibration @ git+https://github.com/defreitasL/IHSetHansonKraus1991.git',
+        'IHSetCalibration @ git+https://github.com/IHCantabria/IHSetUtils.git'
     ],
     author='Lucas de Freitas Pereira',
     author_email='lucas.defreitas@unican.es',
