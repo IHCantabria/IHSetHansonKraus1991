3f9f8eae6a85ea615a2c42b86ff8554b2a43034a
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-04-03 09:53:44 +0200

    Starting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetHansonKraus1991',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -12,7 +12,7 @@ setup(
         'datetime',
         'spotpy',
         'IHSetCalibration @ git+https://github.com/defreitasL/IHSetCalibration.git',
-        'IHSetCalibration @ git+https://github.com/IHCantabria/IHSetUtils.git'
+        'IHSetUtils @ git+https://github.com/IHCantabria/IHSetUtils.git'
     ],
     author='Lucas de Freitas Pereira',
     author_email='lucas.defreitas@unican.es',
