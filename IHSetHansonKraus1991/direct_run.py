3dc58b9565db3dcd2c62ea705af5222813ec8fde
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-06-24 12:45:15 +0200

    Correcting angles and depth bug

--- IHSetHansonKraus1991/direct_run.py
+++ IHSetHansonKraus1991/direct_run.py
@@ -87,7 +87,7 @@ class HansonKraus1991_run(object):
         self.interp_forcing()
         self.split_data()
         
-        self.yi = self.Obs[0,:]
+        self.yi = np.mean(self.Obs_splited_, axis=0)
 
         mkIdx = np.vectorize(lambda t: np.argmin(np.abs(self.time - t)))
         self.idx_obs = mkIdx(self.time_obs)
